8088c7188cbbdcfef1239e5f210072e3242f3c0c
Norma Verónica Trinidad <nvth28@gmail.com>
2021-03-16 03:31:05 -0600

    add events calendar

--- src/app/eventos/eventos.component.ts
+++ src/app/eventos/eventos.component.ts
@@ -1,18 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { DashboardService } from '../services/dashboard.service';
-import { AlertController, ModalController } from '@ionic/angular';
 
-import { ChangeDetectionStrategy, ViewChild, TemplateRef } from '@angular/core';
-import { startOfDay, endOfDay, subDays, addDays, endOfMonth, isSameDay, isSameMonth, addHours } from 'date-fns';
+import { ViewChild, TemplateRef } from '@angular/core';
+import { addDays, isSameDay, isSameMonth } from 'date-fns';
 import { Subject } from 'rxjs';
-import { CalendarEvent, CalendarEventAction, CalendarEventTimesChangedEvent, CalendarView } from 'angular-calendar';
+import { CalendarEvent, CalendarView } from 'angular-calendar';
 
 import flatpickr from 'flatpickr';
 import { Spanish } from 'flatpickr/dist/l10n/es';
 
 const colors: any = {
   red: {
-    primary: '#ad2121',
+    primary: '#f70808',
     secondary: '#FAE3E3',
   },
   blue: {
@@ -23,9 +21,15 @@ const colors: any = {
     primary: '#e3bc08',
     secondary: '#FDF1BA',
   },
+  green: {
+    primary: '#75e208',
+    secondary: '#CFE8B5',
+  },
+  purple: {
+    primary: '#CD6EE7',
+    secondary: '#DDB5E8',
+  },
 };
-
-import { environment } from '@env/environment';
 
 @Component({
   selector: 'app-eventos',
@@ -43,79 +47,60 @@ export class EventosComponent implements OnInit {
 
   viewDate: Date = new Date();
 
-  modalData: {
-    action: string;
-    event: CalendarEvent;
-  };
-
-  actions: CalendarEventAction[] = [
-    {
-      label: '<i class="fas fa-fw fa-pencil-alt"> Editar</i>',
-      a11yLabel: 'Edit',
-      onClick: ({ event }: { event: CalendarEvent }): void => {
-        this.handleEvent('Edited', event);
-      },
-    },
-    {
-      label: '<i class="fas fa-fw fa-trash-alt"> Eliminar</i>',
-      a11yLabel: 'Delete',
-      onClick: ({ event }: { event: CalendarEvent }): void => {
-        this.events = this.events.filter((iEvent) => iEvent !== event);
-        this.handleEvent('Deleted', event);
-      },
-    },
-  ];
-
   refresh: Subject<any> = new Subject();
 
-  events: CalendarEvent[] = [
+  events: any[] = [
     {
-      start: subDays(startOfDay(new Date()), 1),
-      end: addDays(new Date(), 1),
-      title: 'Un evento de 3 días',
+      firstVisit: null,
+      secondVisit: null,
+      title: 'Par de nodos 1',
       color: colors.red,
-      actions: this.actions,
-      allDay: true,
-      resizable: {
-        beforeStart: true,
-        afterEnd: true,
-      },
-      draggable: true,
+      degradedNode: null,
+      notDegradedNode: null,
     },
     {
-      start: startOfDay(new Date()),
-      title: 'Un evento sin fecha de finalización',
-      color: colors.yellow,
-      actions: this.actions,
-    },
-    {
-      start: subDays(endOfMonth(new Date()), 3),
-      end: addDays(endOfMonth(new Date()), 3),
-      title: 'Un evento de dos meses',
+      firstVisit: null,
+      secondVisit: null,
+      title: 'Par de nodos 2',
       color: colors.blue,
-      allDay: true,
+      degradedNode: null,
+      notDegradedNode: null,
     },
     {
-      start: addHours(startOfDay(new Date()), 2),
-      end: addHours(new Date(), 2),
-      title: 'Evento draggable & resizable',
+      firstVisit: null,
+      secondVisit: null,
+      title: 'Par de nodos 3',
       color: colors.yellow,
-      actions: this.actions,
-      resizable: {
-        beforeStart: true,
-        afterEnd: true,
-      },
-      draggable: true,
+      degradedNode: null,
+      notDegradedNode: null,
+    },
+    {
+      firstVisit: null,
+      secondVisit: null,
+      title: 'Par de nodos 4',
+      color: colors.green,
+      degradedNode: null,
+      notDegradedNode: null,
+    },
+    {
+      firstVisit: null,
+      secondVisit: null,
+      title: 'Par de nodos 5',
+      color: colors.purple,
+      degradedNode: null,
+      notDegradedNode: null,
     },
   ];
 
-  activeDayIsOpen: boolean = true;
+  calendarEvents: CalendarEvent[] = [];
 
-  constructor(
-    private alertController: AlertController,
-    private dashboardService: DashboardService,
-    private modalController: ModalController
-  ) {}
+  degradedNodes = ['3_92_0_1336', '3_92_0_1339', '3_92_0_1341', '3_92_0_1342', '3_92_0_1343'];
+
+  notDegradedNodes = ['3_92_1_1334', '3_92_1_1335', '3_92_1_1337', '3_92_1_1338', '3_92_1_1340'];
+
+  activeDayIsOpen: boolean = false;
+
+  constructor() {}
 
   dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
     if (isSameMonth(date, this.viewDate)) {
@@ -126,46 +111,6 @@ export class EventosComponent implements OnInit {
       }
       this.viewDate = date;
     }
-  }
-
-  eventTimesChanged({ event, newStart, newEnd }: CalendarEventTimesChangedEvent): void {
-    this.events = this.events.map((iEvent) => {
-      if (iEvent === event) {
-        return {
-          ...event,
-          start: newStart,
-          end: newEnd,
-        };
-      }
-      return iEvent;
-    });
-    this.handleEvent('Dropped or resized', event);
-  }
-
-  handleEvent(action: string, event: CalendarEvent): void {
-    this.modalData = { event, action };
-    // this.modal.open(this.modalContent, { size: 'lg' });
-  }
-
-  addEvent(): void {
-    this.events = [
-      ...this.events,
-      {
-        title: 'Nuevo evento',
-        start: startOfDay(new Date()),
-        end: endOfDay(new Date()),
-        color: colors.red,
-        draggable: true,
-        resizable: {
-          beforeStart: true,
-          afterEnd: true,
-        },
-      },
-    ];
-  }
-
-  deleteEvent(eventToDelete: CalendarEvent) {
-    this.events = this.events.filter((event) => event !== eventToDelete);
   }
 
   setView(view: CalendarView) {
@@ -183,5 +128,42 @@ export class EventosComponent implements OnInit {
 
   ngOnInit() {
     this.flatpickrFactory();
+  }
+
+  eventChanged() {
+    this.calendarEvents = [];
+    this.events.forEach((event) => {
+      if (!this.eventCompleted(event)) {
+        return;
+      }
+
+      if (event.firstVisit) {
+        const first: CalendarEvent = {
+          start: event.firstVisit,
+          end: addDays(new Date(event.firstVisit), 29),
+          title: `${event.title} - Visita seca`,
+          color: event.color,
+          allDay: true,
+        };
+
+        this.calendarEvents.push(first);
+      }
+
+      if (event.secondVisit) {
+        const second: CalendarEvent = {
+          start: event.secondVisit,
+          end: addDays(new Date(event.secondVisit), 29),
+          title: `${event.title} - Visita lluvia`,
+          color: event.color,
+          allDay: true,
+        };
+
+        this.calendarEvents.push(second);
+      }
+    });
+  }
+
+  eventCompleted(event: any) {
+    return event.degradedNode && event.notDegradedNode && (event.firstVisit || event.secondVisit);
   }
 }
